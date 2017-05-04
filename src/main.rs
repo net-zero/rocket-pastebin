1b4ef6d4961572e9e88d0fdc3a34ed28ec066a37
net-zero <net-zero@users.noreplay.github.com>
2017-05-04 22:28:24 +0800

    change: remove old code

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,6 @@ extern crate serde_json;
 #[macro_use]
 extern crate rocket_contrib;
 
-use std::io;
-use std::path::Path;
-use std::fs::File;
-
-use rocket::Data;
-
 use diesel::pg::PgConnection;
 use r2d2::Pool;
 use r2d2_diesel::ConnectionManager;
@@ -36,9 +30,6 @@ mod controllers;
 #[cfg(test)]
 mod tests;
 
-mod paste_id;
-use paste_id::PasteID;
-
 use controllers::auth;
 use controllers::user;
 use controllers::paste;
@@ -48,46 +39,9 @@ lazy_static! {
     pub static ref DB_POOL: Pool<ConnectionManager<PgConnection>> = helpers::db::create_db_pool();
 }
 
-
-#[get("/")]
-fn index() -> &'static str {
-    "
-    USAGE
-
-        POST /
-
-            accepts raw data in the body of the request and responds with a URL of
-            a page containing the body's content
-
-        GET /<id>
-
-            retrieves the content for the paste with id `<id>`
-    "
-}
-
-#[post("/", data = "<paste>")]
-fn upload(paste: Data) -> io::Result<String> {
-    let id = PasteID::new(3);
-    let filename = format!("upload/{id}", id = id);
-    let url = format!("{host}/{id}\n", host = "http://localhost:8000", id = id);
-
-    // Write the paste out to the file and return the URL.
-    paste.stream_to_file(Path::new(&filename))?;
-    Ok(url)
-}
-
-#[get("/<id>")]
-fn retrieve(id: PasteID) -> Option<File> {
-    let filename = format!("upload/{id}", id = id);
-    File::open(&filename).ok()
-}
-
 fn rocket() -> rocket::Rocket {
     rocket::ignite().mount("/",
-                           routes![index,
-                                   upload,
-                                   retrieve,
-                                   auth::login,
+                           routes![auth::login,
                                    user::me,
                                    user::get_users,
                                    user::create_user,
